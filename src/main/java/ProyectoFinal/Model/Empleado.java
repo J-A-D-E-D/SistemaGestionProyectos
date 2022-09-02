7cbcfbbfde327a0bf38718761d8fe499892ee3bc
Diyued <diegorinconch2607@gmail.com>
2022-09-01 21:12:01 -0500

    Revert "Se agrega el @Id"
    This reverts commit 4584a9cf24521e5f1b575f13ca261f062fc4d4fd.

--- src/main/java/ProyectoFinal/Model/Empleado.java
+++ src/main/java/ProyectoFinal/Model/Empleado.java
@@ -2,13 +2,11 @@ package ProyectoFinal.Model;
 
 import lombok.Data;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 
 
 @Data
 @Entity
 public class Empleado {
-    @Id
     private String nombreEmpleado;
     private String correoEmpleado;
     private String empresaEmpleado;
