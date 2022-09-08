b3b877a7fae9d292416c53d988e562feeeb62f3a
Diyued <diegorinconch2607@gmail.com>
2022-09-07 23:34:22 -0500

    Se agregan los servicios de perfil y el controlador de movimientoDinero

---
+++ src/main/java/com/jaded/sistemagestionproyectos/service/implement/PerfilServiceImpl.java
@@ -0,0 +1,18 @@
+package com.jaded.sistemagestionproyectos.service.implement;
+
+import com.jaded.sistemagestionproyectos.model.perfil;
+import com.jaded.sistemagestionproyectos.repository.Igeneric;
+import com.jaded.sistemagestionproyectos.repository.Iperfil;
+import com.jaded.sistemagestionproyectos.service.IperfilService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PerfilServiceImpl extends crudImp<perfil, Integer> implements IperfilService {
+    @Autowired
+    private Iperfil repo;
+    @Override
+    protected Igeneric<perfil, Integer> getRepo() {
+        return repo;
+    }
+}
