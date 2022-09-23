ce028004fd9c1f0ed782fcd7c21453c403537ca3
Andres Saez <andres.saez159@gmail.com>
2022-09-22 23:32:33 -0500

    Empresa controller

--- src/main/java/com/jaded/sistemagestionproyectos/controller/empresaController.java
+++ src/main/java/com/jaded/sistemagestionproyectos/controller/empresaController.java
@@ -1,6 +1,10 @@
 package com.jaded.sistemagestionproyectos.controller;
+import com.jaded.sistemagestionproyectos.service.IempleadoService;
+import com.jaded.sistemagestionproyectos.service.IempresaService;
 import com.jaded.sistemagestionproyectos.service.implement.empresaImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class empresaController {
     @Autowired
-    private empresaImpl service;
+    private IempresaService service;
 
     /*@Autowired
     @Qualifier("empresaControllerMapper")
-    private ModelMapper mapper;
-*/
+    private ModelMapper mapper;*/
+
     @GetMapping("/empresa")
-    public String tablaEmpresas (Model model) {
-       //model.addAttribute("listaEmpresas",service.mostrarEmpresas());
-        return "0hola";
+    public String tablaEmpresas (Model model) throws Exception {
+       model.addAttribute("listaEmpresas",service.readAll());
+        return "empresas";
     }
 }
